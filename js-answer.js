858fc3dffddb60a8cd6bd1309f294d4352c5c814
Joseph Star <90820348+Private-Account-ID@users.noreply.github.com>
2022-10-25 13:28:01 +0530

    Add files via upload

---
+++ js-answer.js
@@ -0,0 +1,240 @@
+function A1(){
+  document.getElementById('A1').style="display:block";
+}
+function A1H(){
+  document.getElementById('A1').style="display:none";
+}
+function A2(){
+  document.getElementById('A2').style="display:block";
+}
+function A2H(){
+  document.getElementById('A2').style="display:none";
+}
+function A3(){
+  document.getElementById('A3').style="display:block";
+}
+function A3H(){
+  document.getElementById('A3').style="display:none";
+}
+function A4(){
+  document.getElementById('A4').style="display:block";
+}
+function A4H(){
+  document.getElementById('A4').style="display:none";
+}
+function A5(){
+  document.getElementById('A5').style="display:block";
+}
+function A5H(){
+  document.getElementById('A5').style="display:none";
+}
+function A6(){
+  document.getElementById('A6').style="display:block";
+}
+function A6H(){
+  document.getElementById('A6').style="display:none";
+}
+function A7(){
+  document.getElementById('A7').style="display:block";
+}
+function A7H(){
+  document.getElementById('A7').style="display:none";
+}
+function A8(){
+  document.getElementById('A8').style="display:block";
+}
+function A8H(){
+  document.getElementById('A8').style="display:none";
+}
+function A9(){
+  document.getElementById('A9').style="display:block";
+}
+function A9H(){
+  document.getElementById('A9').style="display:none";
+}
+function A10(){
+  document.getElementById('A10').style="display:block";
+}
+function A10H(){
+  document.getElementById('A10').style="display:none";
+}
+function A11(){
+  document.getElementById('A11').style="display:block";
+}
+function A11H(){
+  document.getElementById('A11').style="display:none";
+}
+function A12(){
+  document.getElementById('A12').style="display:block";
+}
+function A12H(){
+  document.getElementById('A12').style="display:none";
+}
+function A13(){
+  document.getElementById('A13').style="display:block";
+}
+function A13H(){
+  document.getElementById('A13').style="display:none";
+}
+function A14(){
+  document.getElementById('A14').style="display:block";
+}
+function A14H(){
+  document.getElementById('A14').style="display:none";
+}
+function A15(){
+  document.getElementById('A15').style="display:block";
+}
+function A15H(){
+  document.getElementById('A15').style="display:none";
+}
+function A16(){
+  document.getElementById('A16').style="display:block";
+}
+function A16H(){
+  document.getElementById('A16').style="display:none";
+}
+function A17(){
+  document.getElementById('A17').style="display:block";
+}
+function A17H(){
+  document.getElementById('A17').style="display:none";
+}
+function A18(){
+  document.getElementById('A18').style="display:block";
+}
+function A18H(){
+  document.getElementById('A18').style="display:none";
+}
+function A19(){
+  document.getElementById('A19').style="display:block";
+}
+function A19H(){
+  document.getElementById('A19').style="display:none";
+}
+function A20(){
+  document.getElementById('A20').style="display:block";
+}
+function A20H(){
+  document.getElementById('A20').style="display:none";
+}
+function A21(){
+  document.getElementById('A21').style="display:block";
+}
+function A21H(){
+  document.getElementById('A21').style="display:none";
+}
+function A22(){
+  document.getElementById('A22').style="display:block";
+}
+function A22H(){
+  document.getElementById('A22').style="display:none";
+}
+function A23(){
+  document.getElementById('A23').style="display:block";
+}
+function A23H(){
+  document.getElementById('A23').style="display:none";
+}
+function A24(){
+  document.getElementById('A24').style="display:block";
+}
+function A24H(){
+  document.getElementById('A24').style="display:none";
+}
+function A25(){
+  document.getElementById('A25').style="display:block";
+}
+function A25H(){
+  document.getElementById('A25').style="display:none";
+}
+function A26(){
+  document.getElementById('A26').style="display:block";
+}
+function A26H(){
+  document.getElementById('A26').style="display:none";
+}
+function A27(){
+  document.getElementById('A27').style="display:block";
+}
+function A27H(){
+  document.getElementById('A27').style="display:none";
+}
+function A28(){
+  document.getElementById('A28').style="display:block";
+}
+function A28H(){
+  document.getElementById('A28').style="display:none";
+}
+function A29(){
+  document.getElementById('A29').style="display:block";
+}
+function A29H(){
+  document.getElementById('A29').style="display:none";
+}
+function A30(){
+  document.getElementById('A30').style="display:block";
+}
+function A30H(){
+  document.getElementById('A30').style="display:none";
+}
+function A31(){
+  document.getElementById('A31').style="display:block";
+}
+function A31H(){
+  document.getElementById('A31').style="display:none";
+}
+function A32(){
+  document.getElementById('A32').style="display:block";
+}
+function A32H(){
+  document.getElementById('A32').style="display:none";
+}
+function A33(){
+  document.getElementById('A33').style="display:block";
+}
+function A33H(){
+  document.getElementById('A33').style="display:none";
+}
+function A34(){
+  document.getElementById('A34').style="display:block";
+}
+function A34H(){
+  document.getElementById('A34').style="display:none";
+}
+function A35(){
+  document.getElementById('A35').style="display:block";
+}
+function A35H(){
+  document.getElementById('A35').style="display:none";
+}
+function A36(){
+  document.getElementById('A36').style="display:block";
+}
+function A36H(){
+  document.getElementById('A36').style="display:none";
+}
+function A37(){
+  document.getElementById('A37').style="display:block";
+}
+function A37H(){
+  document.getElementById('A37').style="display:none";
+}
+function A38(){
+  document.getElementById('A38').style="display:block";
+}
+function A38H(){
+  document.getElementById('A38').style="display:none";
+}
+function A39(){
+  document.getElementById('A39').style="display:block";
+}
+function A39H(){
+  document.getElementById('A39').style="display:none";
+}
+function A40(){
+  document.getElementById('A40').style="display:block";
+}
+function A40H(){
+  document.getElementById('A40').style="display:none";
+}
